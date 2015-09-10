ac444fa2fff6dfd1e72c7cec7701bbe806926d60
Igor Tkach <itkach@aarddict.org>
2015-09-10 09:52:11 -0400

    Generate random letter layout

--- src/LetterBoard.jsx
+++ src/LetterBoard.jsx
@@ -1,7 +1,27 @@
 import React from 'react/addons';
 
-const LETTERS = 'ABCDEFGHIJKLNOPRSTUVWXYZ'.split('');
+const LETTERS = 'ABCDEFGHIJKLNOPRSTUVWXYZ';
 
+// Returns a random integer between min (included) and max (excluded)
+const randomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+const swap = (array, i, j) => {
+  const tmp = array[i];
+  array[i] = array[j];
+  array[j] = tmp;
+};
+
+const randomize = (letterString) => {
+  const result = letterString.split(''),
+        len = result.length;
+  for (let i = 0; i < len - 2; i++) {
+    const randomIndex = randomInt(i, len);
+    swap(result, randomIndex, i);
+  }
+  return result;
+};
 
 const TR = React.createClass({
 
@@ -60,20 +80,50 @@ const LetterRow = React.createClass({
 });
 
 
+const generate = () => {
+  const large = randomize(LETTERS),
+        small = randomize(LETTERS),
+        count = large.length,
+        result = [];
+
+  for (let i = 0; i < count; i++) {
+    result.push(large[i]);
+    result.push(small[i]);
+  }
+  return result;
+};
+
+
 export default React.createClass({
 
   mixins: [
     React.addons.PureRenderMixin
   ],
 
+  getInitialState() {
+    return {
+      letters: generate(),
+      columnCount: 12
+    };
+  },
+
   render: function() {
+
+    const letters = this.state.letters,
+          count = letters.length,
+          columnCount = this.state.columnCount,
+          rowCount = Math.floor(count / columnCount) + (count % columnCount === 0 ? 0 : 1),
+          rows = [];
+
+    for (let i = 0; i < rowCount; i++) {
+      const rowLetters = letters.slice(i * columnCount, (i + 1) * columnCount);
+      rows.push(<LetterRow letters={rowLetters} />);
+    }
+
     return (
       <table>
         <tbody>
-          <LetterRow letters={LETTERS.slice(0, 12)} />
-          <LetterRow letters={LETTERS.slice(6, 18)} />
-          <LetterRow letters={LETTERS.slice(12, 24)} />
-          <LetterRow letters={LETTERS.slice(3, 15)} />
+          {rows}
         </tbody>
       </table>
     );
