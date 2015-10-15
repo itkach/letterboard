bbf74926f1d3a4836c57ca55688bd9ace19ae9b3
Igor Tkach <itkach@aarddict.org>
2015-10-14 20:59:52 -0400

    Move stores and actions into LetterBoardApp.jsx to match structure of HandBoardApp.jsx

--- src/LetterBoardApp.jsx
+++ src/LetterBoardApp.jsx
@@ -4,9 +4,6 @@ import Reflux from 'reflux';
 import keymaster from 'keymaster';
 import Icon from 'react-fontawesome';
 import LetterBoard from './LetterBoard.jsx';
-import Actions from './actions';
-import Store from './store';
-import {Profiles} from './store';
 import QRCode from './QRCode.jsx';
 import If from './If.jsx';
 import run from './run';
@@ -21,6 +18,240 @@ import {
   Modal,
   Alert
 } from 'react-bootstrap';
+
+import randomize from './randomize';
+import localstorage from './localstorage';
+import uuid from 'uuid';
+
+
+const Actions = Reflux.createActions([
+  'setFontSize',
+  'setFontFamily',
+  'setLetterVSpacing',
+  'setLetterHSpacing',
+  'setLetterSet',
+  'setOverlayColor',
+  'regenerate',
+  'setProfile',
+  'saveProfile',
+  'deleteProfile',
+  'lockProfile',
+  'unlockProfile'
+]);
+
+
+const storage = localstorage('letterboard');
+
+
+const generate = letterSet => {
+  const large = randomize(letterSet),
+        small = randomize(letterSet),
+        count = large.length,
+        result = [];
+
+  for (let i = 0; i < count; i++) {
+    result.push(large[i]);
+    result.push(small[i]);
+  }
+  return result;
+};
+
+
+const profileStorageKey = id => 'p:' + id,
+      DEFAULT_PROFILE_ID = 'default';
+
+
+const Profiles = Reflux.createStore({
+
+  listenables: Actions,
+
+  defaults: {
+    current: DEFAULT_PROFILE_ID,
+    available: {[DEFAULT_PROFILE_ID]: 'Default'}
+  },
+
+  init() {
+    const data = storage.get('profiles', {});
+    this.data = {...this.defaults, ...data};
+  },
+
+  getInitialState() {
+    return this.data;
+  },
+
+  get data() {
+    return this._data;
+  },
+
+  set data(newValue) {
+    const available = newValue.available;
+    if (!available ||
+        Object.keys(available).length === 0) {
+          this._data = {...this.defaults};
+    }
+    else {
+      this._data = newValue;
+    }
+    storage.set('profiles', this._data);
+    this.trigger(this._data);
+  },
+
+  onSetProfile(id) {
+    this.data = {...this.data, current: id};
+  },
+
+  onSaveProfile(name, settings) {
+    const id = uuid.v4();
+    storage.set(profileStorageKey(id), settings);
+    const available = {...this.data.available, [id]: name};
+    this.data = {...this.data, available, current: id};
+  },
+
+  onDeleteProfile(id) {
+    storage.remove(profileStorageKey(id));
+    const available = {...this.data.available};
+    delete available[id];
+    this.data = {...this.data, available, current: Object.keys(available)[0]};
+  }
+
+});
+
+
+const Store = Reflux.createStore({
+
+  LETTER_SETS: [
+    'ABCDEFGHIJKLNOPRSTUVWXYZ',
+    'АБВГДЕЖЗИКЛМНОПРСТУФХЧШЯ',
+    'ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ',
+    'ԺԻԽԾԿՀՁՂՃՄՅՆՇՈՉՊՋՍՎՐՑՒՔՖ',
+    'ႠႡႢႣႥႦႧႨႩႫႬႭႮႰႱႲႵႹႻႼႾႿჁჄ',
+    'अइऊऋऌएऑकखगघङचछजझञटठडढणतथ',
+    'オカキクケコシスセソタテトナニヌノヒフヘホマミヨ',
+    'תשרקץצףפעסנמלךכטחזוהדגבא',
+    'กขคงจฉชฑดถทธนบผมยรลวศหอฯ',
+    'ፏሃለሐመሠረሰየቀጦበቧቨተግኀነአከወዐዠጠ',
+    'ᚠᚢᚦᚨᚱᚳᚷᚹᚻᛇᛈᛉᛏᛒᛖᛗᛚᛟᛝᛞᚣᛡᛠᚾ'
+  ],
+
+  FONT_FAMILIES: [
+    'sans-serif',
+    'serif',
+    'monospace'
+  ],
+
+  OVERLAY_COLORS: [
+    null,
+    'red',
+    'green',
+    'black'
+  ],
+
+  listenables: Actions,
+
+  init() {
+
+    const letterSet = this.LETTER_SETS[0];
+
+    this.defaults = {
+      letters: generate(letterSet),
+      letterSet,
+      columnCount: 12,
+      fontSize: 20,
+      letterHSpacing: 0,
+      letterVSpacing: 0,
+      fontFamily: 'serif',
+      overlayColor: null
+    };
+
+    this._onProfileChange(Profiles.getInitialState());
+    this.listenTo(Profiles, this._onProfileChange);
+  },
+
+  getInitialState() {
+    return this.data;
+  },
+
+  get storageKey() {
+    return profileStorageKey(this.profiles.current);
+  },
+
+  get data() {
+    return this._data;
+  },
+
+  set data(newValue) {
+    this._data = newValue;
+    storage.set(this.storageKey, this._data);
+    this.trigger(this._data);
+  },
+
+  _onProfileChange(profiles) {
+    console.debug('Profiles changed', profiles);
+    this.profiles = profiles;
+    this.loadCurrentProfile();
+  },
+
+  loadCurrentProfile() {
+    const data = storage.get(this.storageKey, {});
+    this.data = {...this.defaults, ...data};
+  },
+
+  onRegenerate() {
+    this.data = {...this.data, letters: generate(this.data.letterSet)};
+  },
+
+  onSetFontSize(fontSize) {
+    if (this.data.locked) {
+      return;
+    }
+    this.data = {...this.data, fontSize};
+  },
+
+  onSetFontFamily(fontFamily) {
+    if (this.data.locked) {
+      return;
+    }
+    this.data = {...this.data, fontFamily};
+  },
+
+  onSetLetterVSpacing(letterVSpacing) {
+    if (this.data.locked) {
+      return;
+    }
+    this.data = {...this.data, letterVSpacing};
+  },
+
+  onSetLetterHSpacing(letterHSpacing) {
+    if (this.data.locked) {
+      return;
+    }
+    this.data = {...this.data, letterHSpacing};
+  },
+
+  onSetLetterSet(letterSet) {
+    if (this.data.locked) {
+      return;
+    }
+    const letters = generate(letterSet);
+    this.data = {...this.data, letterSet, letters};
+  },
+
+  onSetOverlayColor(overlayColor) {
+    if (this.data.locked) {
+      return;
+    }
+    this.data = {...this.data, overlayColor};
+  },
+
+  onLockProfile() {
+    this.data = {...this.data, locked: true};
+  },
+
+  onUnlockProfile() {
+    this.data = {...this.data, locked: false};
+  }
+
+});
 
 
 const defaultKeyFilter = keymaster.filter;
